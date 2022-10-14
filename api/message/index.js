655cf57cee42b49886717447636ee71034754c8f
Mike Bendorf <mikebendorf11@gmail.com>
2022-10-14 11:41:56 -0700

    function log statements

--- api/message/index.js
+++ api/message/index.js
@@ -1,9 +1,13 @@
 import fetch from 'node-fetch'
 
-export const httpTrigger =  async(context) => {
-  let r = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  let response = await r.json()
-  context.res.json(response)
+export const httpTrigger =  async(context, req) => {
+  try{
+    context.log(req)
+    let r = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let response = await r.json()
+    context.res.json(response)
+  } catch (e) {
+    context.log(e)
+  }
   context.done = () => {}
-
 }
