25f72f33514979372b86406bbe35fce737a3bd26
Mike Bendorf <mikebendorf11@gmail.com>
2022-10-14 10:51:33 -0700

    renable playwright and add context.done to func

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,9 @@
 import fetch from 'node-fetch'
 
-export const httpTrigger =  async (context) => {
+export const httpTrigger =  async(context) => {
   let r = await fetch('https://jsonplaceholder.typicode.com/todos/1')
   let response = await r.json()
   context.res.json(response)
+  context.done = () => {}
+
 }
