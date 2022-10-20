7a7bafa32616eefa5ecd93855a5c9ecd4dcfa979
Mike Bendorf <mikebendorf11@gmail.com>
2022-10-19 21:25:04 -0700

    reactivate log statement

--- api/message/index.js
+++ api/message/index.js
@@ -2,12 +2,12 @@ import fetch from 'node-fetch';
 
 export default async(context, req) => {
   try{
-    //context.log(req)
+    context.log('log: fetching jsonplaceholder')
     let r = await fetch('https://jsonplaceholder.typicode.com/todos/1')
     let response = await r.json()
     context.res.json(response)
   } catch (e) {
-    //context.log(e)
+    context.log(e)
     context.res.status(500).send(e)
   }
   //context.done = () => {}
